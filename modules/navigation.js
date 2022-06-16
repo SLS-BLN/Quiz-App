38ee26fb7bfa87e6479148124d2c220dad133663
Stefan Schlicker <stefanschlicker@posteo.de>
2022-06-16 18:14:50 +0200

    inner fill and stroke of navbar svgs - color changes on click

--- modules/navigation.js
+++ modules/navigation.js
@@ -3,18 +3,29 @@ function navigation() {
   const navLinks = document.querySelectorAll('[data-js=nav-link]');
   const headline = document.querySelector('[data-js=headline]');
   
+  const navIcons = document.querySelectorAll(".icon__nav");
+
   navLinks.forEach(navLink => {
     navLink.addEventListener('click', () => {
       const selectedPage = document.querySelector(
         `[data-js=${navLink.dataset.link}]`
         );
-      pages.forEach(page => {
-        page.classList.remove('current');
+        pages.forEach(page => {
+          page.classList.remove('current');
+        });
+        selectedPage.classList.add('current');
+        headline.textContent = navLink.dataset.headline;
+
+        navIcons.forEach(navIcon => {
+          navIcon.classList.remove("icon--focus");
+        });
+        const selectedIcon = document.querySelector(
+          `[data-ic=${navLink.dataset.link}]`
+          );
+        selectedIcon.classList.add("icon--focus")
+
+
       });
-      selectedPage.classList.add('current');
-      headline.textContent = navLink.dataset.headline;      
-    });
-    console.log(navLink);
     
   });
 }
